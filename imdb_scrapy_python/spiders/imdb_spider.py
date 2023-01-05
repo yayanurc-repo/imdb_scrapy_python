ada9ea0ad795ba01aa3b27c9769e35fa8c82c845
Yaya Nurcahya <v-yaya.nurcahya@anteraja.id>
2023-01-05 12:37:21 +0700

    fixed .gitignore, settings.py and imdb_spider.py

--- imdb_scrapy_python/spiders/imdb_spider.py
+++ imdb_scrapy_python/spiders/imdb_spider.py
@@ -1,3 +1,6 @@
+# text cleaning
+import re
+
 import scrapy
 
 class ImdbSpider(scrapy.Spider):
@@ -7,18 +10,32 @@ class ImdbSpider(scrapy.Spider):
     def parse(self, response):
         # Extract film data
         for film in response.css('div.lister-item'):
+            year = film.css('.lister-item-year::text').get()
+            try:
+                year = int(year.strip('()'))
+            except ValueError:
+                year = int(year.strip('(I)').strip('(II)').replace(' (', ''))
+            year = str(year)
+
             yield {
                 'name': film.css('h3 a::text').get(),
-                'year': film.css('h3 span.lister-item-year::text').get(),
-                'stars': film.css('p.text-muted span.certificate::text').get(),
-                'rank': film.css('div.lister-item-index::text').get(),
-                'duration': film.css('p.text-muted span.runtime::text').get(),
-                'genre': film.css('p.text-muted span.genre::text').get(),
-                'director': film.css('p.text-muted span.credit::text').get(),
-                'synopsis': film.css('p.text-muted span.plot::text').get(),
+                'year': year,
+                'stars': film.css('.ratings-imdb-rating strong::text').get(),
+                'rank': film.css('.lister-item-index::text').get().strip('.'),
+                'duration': film.css('.runtime::text').get(),
+                'genre': film.css('.genre::text').get().strip(),
+                'director': film.css("p:contains('Director') a::text").get(),
+                'synopsis': self.__remove_html_tags__(
+                    film.css('.ratings-bar + p').get()
+                ),
             }
 
         # Follow pagination links
         next_page = response.css('a.lister-page-next::attr(href)').get()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
+
+    def __remove_html_tags__(self, text):
+        """remove html tags from string"""
+        html_tags = re.compile('<.*?>')
+        return re.sub(html_tags, '', text).strip('\n')
